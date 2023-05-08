ce643ecc1ed0799dcde32786d53e3f7ab823e515
kylemocode <kylemo860617@gmail.com>
2023-05-08 21:41:15 +0800

    ✨ page setup (_app, _document)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import DynamicTheme from '@/themes/DynamicTheme';
+import Meta from '@/components/Meta/Meta';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <DynamicTheme>
+      <Meta />
+      <Component {...pageProps} />
+    </DynamicTheme>
+  );
 }
+
+export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,67 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render = () => {
+    return (
+      <Html lang='zh-TW'>
+        <Head
+          prefix='og: http://ogp.me/ns#'
+          itemScope
+          itemType='https://schema.org/WebSite'
+        >
+          <meta charSet='utf-8' />
+          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+
+          <meta
+            name='apple-mobile-web-app-status-bar-style'
+            content='default'
+          />
+
+          <meta name='theme-color' content='#006aa6' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          {/* https://github.com/whatwg/html/issues/4504 */}
+          <meta name='supported-color-schemes' content='light' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  };
 }
