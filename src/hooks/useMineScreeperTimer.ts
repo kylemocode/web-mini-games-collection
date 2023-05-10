90b65bdb278fba54a3fb363728ef706e0a4cdaa0
kylemocode <kylemo860617@gmail.com>
2023-05-10 23:51:23 +0800

    ✨ use timer hooks

---
+++ src/hooks/useMineScreeperTimer.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect, useCallback } from 'react';
+
+import { GameStatus } from '@/components/modules/mineSweeper/config';
+
+function useMineScreeperTimer(status: GameStatus): number {
+  const [seconds, setSeconds] = useState(0);
+
+  const addSecond = useCallback(() => {
+    setSeconds(prevSec => prevSec + 1);
+  }, []);
+
+  useEffect(() => {
+    let timer: number;
+
+    switch (status) {
+      case GameStatus.STARTED:
+        timer = setInterval(addSecond, 1000) as unknown as number;
+        break;
+      case GameStatus.NEW:
+        setSeconds(0);
+        break;
+      default:
+        break;
+    }
+
+    return () => clearInterval(timer);
+  }, [status, addSecond]);
+
+  return seconds;
+}
+
+export default useMineScreeperTimer;
