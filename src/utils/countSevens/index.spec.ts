d172e1ef3f10f412b1fc68e9472e6783dcbbc320
kylemocode <kylemo860617@gmail.com>
2023-05-15 10:22:56 +0800

    ✅ add more test case consition for countSevens util

--- src/utils/countSevens/index.spec.ts
+++ src/utils/countSevens/index.spec.ts
@@ -1,5 +1,7 @@
 import countSevens from '@/utils/countSevens';
 
+import { ERROR_IS_NOT_INTEGER } from './index';
+
 describe('countSevens util function', () => {
   it('should return 0 when n is 0', () => {
     expect(countSevens(0)).toBe(0);
@@ -11,6 +13,15 @@ describe('countSevens util function', () => {
 
   it('should return 0 when n is a positive integer without 7', () => {
     expect(countSevens(5)).toBe(0);
+  });
+
+  it('should return 0 when n is a negative number', () => {
+    expect(countSevens(-7)).toBe(0);
+  });
+
+  it('should throw an error when n is a float', () => {
+    expect(() => countSevens(7.7)).toThrow(ERROR_IS_NOT_INTEGER);
+    expect(() => countSevens(-7.7)).toThrow(ERROR_IS_NOT_INTEGER);
   });
 
   it('should return the correct count of sevens within a specific range', () => {
--- src/utils/countSevens/index.ts
+++ src/utils/countSevens/index.ts
@@ -1,4 +1,10 @@
+export const ERROR_IS_NOT_INTEGER = 'Input must be an integer';
+
 function countSevens(n: number) {
+  if (!Number.isInteger(n)) {
+    throw new Error(ERROR_IS_NOT_INTEGER);
+  }
+
   let count = 0;
 
   for (let i = 1; i <= n; i++) {
