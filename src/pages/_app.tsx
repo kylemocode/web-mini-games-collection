8ca76c564e2fff85d43caebf9426f0855323f5bb
kylemocode <kylemo860617@gmail.com>
2023-07-03 20:57:47 +0800

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,9 @@ import type { AppProps } from 'next/app';
 import DynamicTheme from '@/themes/DynamicTheme';
 import Meta from '@/components/general/Meta/Meta';
 
-if (process.env.NODE_ENV === 'production') {
-  require('@newrelic/next');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   require('@newrelic/next');
+// }
 
 function MyApp({ Component, pageProps }: AppProps) {
   console.log('test');
